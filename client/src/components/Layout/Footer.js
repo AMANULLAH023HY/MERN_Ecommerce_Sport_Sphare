9bad95c4e26702f162715d5a7395740f2cd0b356
AMANULLAH023HY <amana170829@gmail.com>
2024-02-19 22:54:15 +0530

    react Setup & layout this

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='bg-dark text-light p-3'>
+    <h4 className='text-center'>
+    All Right Reserved &copy; Rose</h4>
+    </div>
+  )
+}
