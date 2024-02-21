75bec132636876f3000702b4813630015a94ac40
AMANULLAH023HY <amana170829@gmail.com>
2024-02-21 15:06:10 +0530

    Complete Footer & Page Not Found UI

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-
 function Layout(props){
  return(
     <>
     <Header/>
-    <main style={{minHeight:"85vh"}}>
+    <main style={{minHeight:"79vh"}}>
 
         {props.children}
     </main>
