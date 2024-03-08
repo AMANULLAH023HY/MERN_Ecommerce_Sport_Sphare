be31ec1e8e80ce45b0c39027bb6acfa552994385
AMANULLAH023HY <amana170829@gmail.com>
2024-03-08 15:45:22 +0530

    Get Product in UI

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,7 @@ import CreateProduct from "./pages/admin/CreateProduct";
 import Users from "./pages/admin/Users";
 import Orders from "./pages/user/Orders";
 import Profile from "./pages/user/Profile";
+import Products from "./pages/admin/Products";
 
 function App() {
   return (
@@ -35,6 +36,7 @@ function App() {
           <Route path="admin" element={<AdminDashboard />} />
           <Route path="admin/create-category" element={<CreateCategory />} />
           <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/products" element={<Products />} />
           <Route path="admin/users" element={<Users />} />
         </Route>
         <Route path="/login" element={<Login />} />
