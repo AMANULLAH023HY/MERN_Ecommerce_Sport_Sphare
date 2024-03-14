4566dcdc970be6f782e9ad9dab006b410ff97973
AMANULLAH023HY <amana170829@gmail.com>
2024-03-14 15:27:23 +0530

    Filter by category and Prices base UI

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,17 @@
 
 import React, { useEffect, useState } from 'react'
 import Layout from '../components/Layout/Layout'
-import { useAuth } from '../context/Auth'
+
 import axios from 'axios'
 import toast from 'react-hot-toast'
-import {Checkbox} from 'antd'
+import {Checkbox, Radio} from 'antd'
+import { Prices } from '../components/Prices'
 
 export default function HomePage() {
-  const [auth, setAuth] = useAuth();
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [checked , setChecked] = useState([]);
+  const [ radio , setRadio] = useState([]);
 
   //get all cat
   const getAllCategory = async () => {
@@ -43,9 +45,24 @@ export default function HomePage() {
     }
   };
 
+
+  // filter by category 
+  const handleFilter = async(value, id)=>{
+
+const all = [...checked];
+if(value){
+  all.push(id)
+}else{
+  all = all.filter(e => c!== id)
+}
+setChecked(all);
+
+
+  }
+  
   useEffect(()=>{
     getAllProducts();
-  },[])
+  },[]);
 
   return (
 <Layout title={'All Product - Best offers'}>
@@ -54,15 +71,31 @@ export default function HomePage() {
   <h4 className='text-center'>Filter by category</h4>
   <div className='d-flex flex-column'>
   {categories?.map(c=>(
-    <Checkbox key={c._id} onChange={(e)=>console.log(e)}>
+    <Checkbox key={c._id} onChange={(e)=> handleFilter(e.target.checked, c._id)}>
     
       {c.name}
     </Checkbox>
   ))}
   </div>
 
+{/* Price Filter  */}
+  <h4 className='text-center mt-4'>Filter by Price</h4>
+  <div className='d-flex flex-column'>
+
+  <Radio.Group onChange={(e)=>setRadio(e.target.value)}>
+    {Prices?.map((p)=>(
+      <div key={p._id}>
+
+      <Radio value={p.array}>{p.name} </Radio>
+      </div>
+    ))}
+  </Radio.Group>
+
+  </div>
+
 </div>
 <div className='col-md-9'>
+{JSON.stringify(radio,null,4)}
   <h1 className='text-center'>All Products</h1>
 
   <div className='d-flex flex-wrap'>
