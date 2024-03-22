017f88d744e4558f218522f866336e67189aa535
AMANULLAH023HY <amana170829@gmail.com>
2024-03-22 16:18:39 +0530

    Remove to cart items

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -7,6 +7,20 @@ const CartPage = () => {
   const [cart, setCart] = useCart();
   const [auth, setAuth] = useAuth();
   const naviagte = useNavigate();
+
+//   dlete item 
+const removeCartItem = (pid)=>{
+    try {
+        let myCart = [...cart];
+        let index = myCart.findIndex(item=>item._id === pid);
+        myCart.splice(index,1);
+        setCart(myCart)
+        
+    } catch (error) {
+        console.log(error)
+    }
+
+}
   return (
     <Layout>
       <div className="container">
@@ -23,6 +37,30 @@ const CartPage = () => {
                 : "Your cart is empty"}
             </h4>
           </div>
+        </div>
+        <div className="row">
+          <div className="col-md-8">
+            {cart?.map((p) => (
+              <div className="row mb-2 p-3 card flex-row">
+                <div className="col-md-4">
+                  <img
+                    src={`http://localhost:8080/api/v1/product/product-photo/${p._id}`}
+                    className="card-img-top"
+                    alt={p.name}
+                    width={"100px"}
+                    height={"100px"}
+                  />
+                </div>
+                <div className="col-md-8">
+                  <p>{p.name}</p>
+                  <p>{p.description.substring(0, 30)} </p>
+                  <p>Price: {p.price}</p>
+                  <button className="btn btn-danger" onClick={()=>removeCartItem(p._id)}>Remove</button>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="col-md-4">Checkout | Payment</div>
         </div>
       </div>
     </Layout>
